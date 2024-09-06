356bfac0f904e6e05b440a32ffd924e13cd6b839
raphamigliorini <raphael.mig@outlook.com>
2024-09-06 14:15:31 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,26 @@
+let dados = [{
+    titulo : "Daenerys Targeryan",
+    descricao : "Exilada desde a infância, ela cresce em Essos, carregando o peso da história de sua família e a esperança de reconquistar o Trono de Ferro. Daenerys é uma figura controversa, capaz de atos de grande crueldade e compaixão, e sua jornada é marcada por perdas, traições e a busca por poder. A personagem, conhecida por sua força de vontade e seus três dragões, se tornou um símbolo de empoderamento feminino e de luta por justiça.",
+    link : "https://pt.wikipedia.org/wiki/Daenerys_Targaryen" 
+},
+{
+    titulo: "Aerys II Targaryen",
+    descricao: "Conhecido como o 'Rei Louco', Aerys II Targaryen foi o último rei da dinastia Targaryen a governar os Sete Reinos.",
+    link: "https://pt.wikipedia.org/wiki/Aerys_II_Targaryen"
+  },
+ {
+    titulo: "Rhaegar Targaryen",
+    descricao: "Príncipe herdeiro e filho mais velho de Aerys II Targaryen, Rhaegar era considerado um homem justo e inteligente.",
+    link: "https://pt.wikipedia.org/wiki/Rhaegar_Targaryen"
+  },
+  {
+    titulo: "Viserys Targaryen I",
+    descricao: "Viserys I Targaryen foi o quinto rei dos Targaryen em Westeros e o avô de Daenerys Targaryen.",
+    link: "https://pt.wikipedia.org/wiki/Viserys_I_Targaryen" 
+  },
+   {
+    titulo: "Rhaenyra Targaryen",
+    descricao: "Primeira mulher a ser nomeada herdeira ao Trono de Ferro, Rhaenyra foi uma figura central na Dança dos Dragões.",
+    link: "https://pt.wikipedia.org/wiki/Rhaenyra_Targaryen" 
+  }
+];
