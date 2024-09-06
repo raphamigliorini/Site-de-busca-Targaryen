f78c71db9d59fa4575f04eb2ec8c326a52290361
raphamigliorini <raphael.mig@outlook.com>
2024-09-06 17:24:56 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,84 @@
+let dados = [{
+    titulo : "Daenerys Targaryen",
+    descricao : "Exilada desde a infância, ela cresce em Essos, carregando o peso da história de sua família e a esperança de reconquistar o Trono de Ferro. Daenerys é uma figura controversa, capaz de atos de grande crueldade e compaixão, e sua jornada é marcada por perdas, traições e a busca por poder. A personagem, conhecida por sua força de vontade e seus três dragões, se tornou um símbolo de empoderamento feminino e de luta por justiça.",
+    link : "https://pt.wikipedia.org/wiki/Daenerys_Targaryen" 
+},
+{
+    titulo: "Aerys II Targaryen",
+    descricao: "Conhecido como o 'Rei Louco', Aerys II Targaryen foi o último rei da dinastia Targaryen a governar os Sete Reinos.",
+    link: "https://pt.wikipedia.org/wiki/Aerys_II_Targaryen"
+  },
+ {
+    titulo: "Rhaegar Targaryen",
+    descricao: "Príncipe herdeiro e filho mais velho de Aerys II Targaryen, Rhaegar era considerado um homem justo e inteligente.",
+    link: "https://pt.wikipedia.org/wiki/Rhaegar_Targaryen"
+  },
+  {
+    titulo: "Viserys Targaryen I",
+    descricao: "Viserys I Targaryen foi o quinto rei dos Targaryen em Westeros e o avô de Daenerys Targaryen.",
+    link: "https://pt.wikipedia.org/wiki/Viserys_I_Targaryen" 
+  },
+   {
+    titulo: "Rhaenyra Targaryen",
+    descricao: "Primeira mulher a ser nomeada herdeira ao Trono de Ferro, Rhaenyra foi uma figura central na Dança dos Dragões.",
+    link: "https://pt.wikipedia.org/wiki/Rhaenyra_Targaryen" 
+  },
+    {
+      titulo: "Aegon I Targaryen",
+      descricao: "Conquistador de Westeros e fundador da dinastia Targaryen. Montou Balerion, o Terror Negro, e, junto com suas irmãs Visenya e Rhaenys, unificou os Sete Reinos.",
+      link: "https://awoiaf.westeros.org/index.php/Aegon_I_Targaryen"
+    },
+    {
+      titulo: "Visenya Targaryen",
+      descricao: "Irmã de Aegon I. Uma das três dragões que conquistaram Westeros.",
+      link: "https://awoiaf.westeros.org/index.php/Visenya_Targaryen"
+    },
+    {
+      titulo: "Rhaenys Targaryen",
+      descricao: "Irmã de Aegon I. Outra das três dragões que conquistaram Westeros.",
+      link: "https://awoiaf.westeros.org/index.php/Rhaenys_Targaryen"
+    } ,
+    {
+        titulo: "Maegor I Targaryen",
+        descricao: "Conhecido por sua crueldade, foi um dos reis Targaryen mais temidos.",
+        link: "https://awoiaf.westeros.org/index.php/Maegor_I_Targaryen"
+      },
+      {
+        titulo: "Jaehaerys I Targaryen",
+        descricao: "Considerado um dos maiores reis Targaryen, conhecido por sua sabedoria e justiça.",
+        link: "https://awoiaf.westeros.org/index.php/Jaehaerys_I_Targaryen"
+      },
+      {
+        titulo: "Viserys I Targaryen",
+        descricao: "Pai de Rhaenyra Targaryen, seu reinado foi marcado pela disputa pela sucessão.",
+        link: "https://awoiaf.westeros.org/index.php/Viserys_I_Targaryen"
+      },
+      {
+        titulo: "Aegon II Targaryen",
+        descricao: "Filho de Viserys I, envolvido na Dança dos Dragões.",
+        link: "https://awoiaf.westeros.org/index.php/Aegon_II_Targaryen"
+      },
+      
+      {
+        titulo: "Aegon III Targaryen",
+        descricao: "Conhecido como Aegon o Jovem, reinou após a Dança dos Dragões.",
+        link: "https://awoiaf.westeros.org/index.php/Aegon_III_Targaryen"
+      },
+      {
+        titulo: "Daemon Targaryen",
+        descricao: "Tio de Aegon II, um guerreiro habilidoso e ambicioso.",
+        link: "https://awoiaf.westeros.org/index.php/Daemon_Targaryen"
+      },
+      {
+        titulo: "Viserys Targaryen (filho de Aerion)",
+        descricao: "Irmão de Daenerys, morreu jovem em Essos.",
+        link: "https://es.wiktionary.org/wiki/removido"
+      },
+      {
+        titulo: "Aerion Targaryen",
+        descricao: "Tio de Daenerys, conhecido por sua loucura e obsessão por dragões.",
+        link: "https://awoiaf.westeros.org/index.php/Aerion_Targaryen"
+      }
+    // ... continue adicionando os outros personagens
+  ]
+
